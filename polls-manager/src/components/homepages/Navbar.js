384180550aa65263c083512788338f78543d9e82
Roy6801 <>
2021-03-21 21:48:01 +0530

    combined

---
+++ polls-manager/src/components/homepages/Navbar.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <html>
+        <body>
+          <nav
+            className="navbar navbar-expand-lg navbar-dark bg-dark"
+            size="500"
+          >
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <NavLink className="nav-brand" exact to="/">
+                    Polls Manager
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" exact to="/dashboard">
+                    Dashboard
+                  </NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink className="nav-link" exact to="/about">
+                    About Us
+                  </NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink className="nav-link" exact to="/contact">
+                    Contacts Us
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </body>
+      </html>
+    );
+  }
+}
+
+export default Navbar;
