c4b877b75b9e19a04df5130b28f036a0470b2492
Roy6801 <>
2021-03-22 11:23:46 +0530

    modified structure

--- Launcher.py
+++ Launcher.py
@@ -1,12 +1,9 @@
 import os
 
-def dependency():
-	os.chdir('./polls-manager')
-	os.system('pip3 install virtualenv')
-	os.system('virtualenv api\\venv')
-	os.system('api\\venv\\Scripts\\pip3 install -r requirements.txt')
-	os.system('npm install')
-
-dependency()
-os.system('start /min cmd /c npm run start-api')
+os.system('pip3 install virtualenv')
+os.system('virtualenv api\\venv')
+os.system('api\\venv\\Scripts\\pip3 install -r requirements.txt')
+os.system('start /min cmd /c api\\venv\\Scripts\\python api\\api.py')
+os.chdir('./polls-manager')
+os.system('npm install')
 os.system('start /min cmd /c npm start')
