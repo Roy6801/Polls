312c7c595fceffdf741dc2498b750c2e97c3a1cb
Roy6801 <>
2021-04-08 21:29:25 +0530

    minor

--- polls-manager/src/App.js
+++ polls-manager/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/login/Login";
 import NavbarDash from "./components/dashboardpages/NavbarDash";
-import Createform from "./components/dashboardpages/Createform";
+import CreateForm from "./components/dashboardpages/CreateForm";
 import Contact from "./components/dashboardpages/Contact";
 import About from "./components/dashboardpages/About";
 import User from "./components/dashboardpages/User";
@@ -30,7 +30,7 @@ const App = () => {
       <div>
         <Switch>
           <Route exact path="/" component={User} />
-          <Route exact path="/createform" component={Createform} />
+          <Route exact path="/createform" component={CreateForm} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
         </Switch>
