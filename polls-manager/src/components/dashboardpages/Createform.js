8d13b6df29c08d2b61a3824ee1f099bab82e41fc
Roy6801 <>
2021-04-14 22:05:47 +0530

    create poll

--- polls-manager/src/components/dashboardpages/Createform.js
+++ polls-manager/src/components/dashboardpages/Createform.js
@@ -4,221 +4,208 @@ import Service from "../Service";
 const CreateForm = () => {
   var multi;
   var storedate;
-  const[anon,setAnon]=useState(true);
-  const [checked,setChecked]= useState(false);
-  const[multicheck,setMulticheck] = useState(false);
+  const [anon, setAnon] = useState(true);
+  const [checked, setChecked] = useState(false);
+  const [multicheck, setMulticheck] = useState(false);
   const [title, setTitle] = useState("");
   const [vCriteria, setVCriteria] = useState("");
-  const [starttime, setStarttime] = useState() ;
+  const [starttime, setStarttime] = useState();
   const [endtime, setEndtime] = useState();
   const [startdate, setStartdate] = useState();
   const [enddate, setEnddate] = useState();
-  const [field, setField] = useState([{value: null}]);
+  const [field, setField] = useState([{ value: null }]);
   const [val, setval] = useState(4);
   const sliderange = (e) => {
     setval(e.target.value);
-}
+  };
 
-const handleStartdate=(e)=>{
-    const begandate= new Date(e.target.value);
-    storedate=begandate.getTime();
-    const startdate = Math.floor(storedate/1000);
+  const handleStartdate = (e) => {
+    const begandate = new Date(e.target.value);
+    storedate = begandate.getTime();
+    const startdate = Math.floor(storedate / 1000);
     setStartdate(startdate);
-}
+  };
 
-const handleEnddate=(e)=>{
-    const begandate= new Date(e.target.value);
-    storedate=begandate.getTime();
-    const enddate = Math.floor(storedate/1000);
+  const handleEnddate = (e) => {
+    const begandate = new Date(e.target.value);
+    storedate = begandate.getTime();
+    const enddate = Math.floor(storedate / 1000);
     setEnddate(enddate);
-}
+  };
 
-const handleStarttime=(e)=>{
+  const handleStarttime = (e) => {
     const begantime = e.target.value;
     const hrs = begantime.split(":")[0];
     const mins = begantime.split(":")[1];
-    const starttime= Number(hrs)*3600 + Number(mins) * 60;
+    const starttime = Number(hrs) * 3600 + Number(mins) * 60;
     setStarttime(starttime);
-}
+  };
 
-const handleEndtime=(e)=>{
+  const handleEndtime = (e) => {
     const begantime = e.target.value;
     const hrs = begantime.split(":")[0];
     const mins = begantime.split(":")[1];
-    const endtime= Number(hrs)*3600 + Number(mins) * 60;
+    const endtime = Number(hrs) * 3600 + Number(mins) * 60;
     setEndtime(endtime);
-}
+  };
 
-const handleInputs = (e,i) =>{
+  const handleInputs = (e, i) => {
     const values = [...field];
-    values[i]=e.target.value;
+    values[i] = e.target.value;
     setField(values);
     console.log(values);
-}
+  };
 
-const handlefield=()=>{
-    var fieldArray=[];
-    for(var i=1; i<=(val);i++) {
-        fieldArray.push (i);
+  const handlefield = () => {
+    var fieldArray = [];
+    for (var i = 1; i <= val; i++) {
+      fieldArray.push(i);
     }
-    return(
-        <div>
-           {fieldArray.map(( i ) => {
-               return(
-                   <div key={i}>
-                    <input
-                    type = "text"
-                    required
-                    placeholder={i+" : Choice"}
-                    onChange={e => handleInputs(e, i)}/>
-                   </div>
-               )
-           })}
-        </div>
-    )
-}
-const handleSchedule=()=>{
-    if (checked)
-    {
-        return (
-        <div>
-        <div className="date-in" >
-                <input type="date" 
-                onChange = {handleStartdate} 
-                />
-        </div>
-        <div className="time-in">
-                <input type="time" 
-                onChange = {handleStarttime} 
-                />
-        </div>
-        </div>
-    )
- }
-}
-
-const handleAnonmity=()=>{
-    if(!anon)
-    {
-        return(
-            <div>
-             <div>
-                 <input type="text" 
-                 placeholder="Enter the verfication type"
-                 onChange={(e) => setVCriteria(e.target.value)}
-                 />
+    return (
+      <div>
+        {fieldArray.map((i) => {
+          return (
+            <div key={i}>
+              <input
+                type="text"
+                required
+                placeholder={i + " : Choice"}
+                onChange={(e) => handleInputs(e, i)}
+              />
             </div>
-            </div>
-        )
+          );
+        })}
+      </div>
+    );
+  };
+  const handleSchedule = () => {
+    if (checked) {
+      return (
+        <div>
+          <div className="date-in">
+            <input type="date" onChange={handleStartdate} />
+          </div>
+          <div className="time-in">
+            <input type="time" onChange={handleStarttime} />
+          </div>
+        </div>
+      );
     }
-}
+  };
 
-    if(multicheck)
-    {
-        multi=1;
+  const handleAnonmity = () => {
+    if (!anon) {
+      return (
+        <div>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter the verfication type"
+              onChange={(e) => setVCriteria(e.target.value)}
+            />
+          </div>
+        </div>
+      );
     }
-    else
-    {
-         multi=0;
-    }
+  };
 
+  if (multicheck) {
+    multi = 1;
+  } else {
+    multi = 0;
+  }
 
-const submitHandler = (e) => {
+  const submitHandler = (e) => {
     e.preventDefault();
     const user = {
-        userName: window.localStorage.getItem("polls-manager-system-G22-user"),
-        pollName: title,
-        verificationCriteria: vCriteria,
-        ts: startdate+starttime,
-        deadline: enddate+endtime,
-        anonymity: anon,
-        scheduled: checked,
-        radio: multi,
-        optionsCount: val,
-        options: field,
-    }
+      userName: window.localStorage.getItem("polls-manager-system-G22-user"),
+      pollName: title,
+      verificationCriteria: vCriteria,
+      ts: startdate + starttime,
+      deadline: enddate + endtime,
+      anonymity: anon,
+      scheduled: checked,
+      radio: multi,
+      optionsCount: val,
+      options: field,
+    };
 
     Service.createPoll(user).then((resp) => {
-        console.log(resp);
+      console.log(resp);
     });
-}
-
+  };
 
   return (
     <div
       className="createform"
-      style={{ height: "100%",
-      backgroundColor: "green" }}
+      style={{ height: "100%", backgroundColor: "green" }}
     >
-      <form onSubmit={(e) => submitHandler(e)} >
-            <div className="titlepoll">
-                
-                <h2>Title</h2>
-                <input type="text" 
-                placeholder="Enter the poll title" 
-                required 
-                onChange={(e)=>setTitle(e.target.value)} 
-                />
-            </div>
+      <form onSubmit={(e) => submitHandler(e)}>
+        <div className="titlepoll">
+          <h2>Title</h2>
+          <input
+            type="text"
+            placeholder="Enter the poll title"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
 
-            <div className="checkbox-obuttons">
-                
-                <input type="checkbox" 
-                defaultChecked={anon} 
-                onChange={()=>anon?setAnon(false):setAnon(true)} 
-                /> Anonymity
+        <div className="checkbox-obuttons">
+          <input
+            type="checkbox"
+            defaultChecked={anon}
+            onChange={() => (anon ? setAnon(false) : setAnon(true))}
+          />{" "}
+          Anonymity
+          <input
+            type="checkbox"
+            defaultChecked={checked}
+            onChange={() => (checked ? setChecked(false) : setChecked(true))}
+          />{" "}
+          Scheduled
+        </div>
 
-                <input type="checkbox" 
-                defaultChecked = {checked} 
-                onChange={()=>checked?setChecked(false):setChecked(true)} 
-                /> Scheduled
-            </div>
+        {handleAnonmity()}
 
-            {handleAnonmity()}
+        {handleSchedule()}
 
-            {handleSchedule()}
+        <div className="date-in">
+          <input type="date" onChange={handleEnddate} required />
+        </div>
 
-            <div className="date-in">
-                <input type="date" 
-                onChange={handleEnddate} 
-                required 
-                />
-            </div>
+        <div className="time-in">
+          <input type="time" onChange={handleEndtime} required />
+        </div>
 
-            <div className="time-in">
-                <input type="time" 
-                onChange={handleEndtime} 
-                required 
-                />
-            </div>
+        <div>
+          <input
+            type="checkbox"
+            defaultChecked={multicheck}
+            onChange={() =>
+              multicheck ? setMulticheck(false) : setMulticheck(true)
+            }
+          />
+          Multi-Candidate Selection
+        </div>
 
-            <div>
-                <input type="checkbox" 
-                defaultChecked={multicheck} 
-                onChange={()=>multicheck?setMulticheck(false):setMulticheck(true)}
-                />Multi-Candidate Selection
-            </div>
+        <div className="range-in">
+          <label> Value:{val}</label>
+          <input
+            type="range"
+            min="2"
+            max="8"
+            className="slider"
+            id="myRange"
+            value={val}
+            onChange={sliderange}
+          />
+        </div>
+        {handlefield()}
 
-            <div className="range-in">
-                <label> Value:{val}</label>
-                <input
-                type="range"
-                min="2"
-                max="8"
-                className="slider"
-                id="myRange"
-                value={val}
-                onChange={sliderange}
-                />
-            </div>
-            {handlefield()}
-
-            <div className="button-n">
-                    <button 
-                    type="submit">
-                        Submit
-                    </button>
-            </div>
+        <div className="button-n">
+          <button type="submit">Submit</button>
+        </div>
       </form>
     </div>
   );
