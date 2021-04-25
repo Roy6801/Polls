7d990b27105a4b5418b45aa0fbbb5d64d6c6b415
RohitRana19 <rohitrana0019@gmail.com>
2021-04-25 17:45:00 +0530

    try2

--- polls-manager/src/components/interface/VerifyForPoll.js
+++ polls-manager/src/components/interface/VerifyForPoll.js
@@ -16,7 +16,7 @@ const VerifyForPoll = ({ pollInfo }) => {
 
   if (!proceed) {
     return (
-      <div className="mainDiv">
+      <div className="mainDiv" style={{background: "linear-gradient(140deg, rgba(248,222,126,1) 39%, rgba(255,253,208,1) 100%)"}}>
         <form onSubmit={handleClick}>
           <h1>Enter Verification ID</h1>
           <h3>Scheduled For : {String(new Date(pollInfo.timestamp * 1000))}</h3>
